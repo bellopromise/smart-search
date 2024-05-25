8a4b5a5a576b03f10c0a9e7451b77a35a893f205
Bello Promise <p.bello@axelor.com>
2024-05-25 18:06:08 +0100

    Added changes for not found and nodemon

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/', router);
 
+// Middleware to handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+// Middleware to handle other errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
